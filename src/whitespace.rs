ab062f3608f1827e27aa6bbdf486f2806f2392fe
Sven Nilsen <bvssvni@gmail.com>
2015-05-05 16:39:16 +0200

    Removed some old code

--- src/whitespace.rs
+++ src/whitespace.rs
@@ -12,26 +12,6 @@ pub struct Whitespace {
     pub optional: bool,
 }
 
-/*
-impl MetaReader for Whitespace {
-    fn start_node(&mut self, name: &str) -> Option<Error> {
-        Some(Error::NotSupported)
-    }
-    fn end_node(&mut self) -> Option<Error> {
-        Some(Error::NotSupported)
-    }
-    fn start_optional(&mut self) -> Option<Error> {
-        Some(Error::NotSupported)
-    }
-    fn end_optional(&mut self) -> Option<Error>;
-    fn start_select(&mut self) -> Option<Error>;
-    fn reset_select(&mut self) -> Option<Error>;
-    fn end_select(&mut self) -> Option<Error>;
-    fn set_as_bool(&mut self, name: &str, val: bool) -> Option<Error>;
-    fn set_as_str(&mut self, name: &str, val: &str) -> Option<Error>;
-}
-*/
-
 /// A hard coded parameter rule for whitespace.
 pub static WHITESPACE: Parameter<'static> = Parameter {
     name: "whitespace",
