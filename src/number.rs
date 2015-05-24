28a7761e112348d44277137373d59e9b6d8d511a
Sven Nilsen <bvssvni@gmail.com>
2015-05-24 12:50:54 +0200

    Added unit tests for `Number`
    Closes https://github.com/PistonDevelopers/meta/issues/61

--- src/number.rs
+++ src/number.rs
@@ -52,3 +52,43 @@ impl Number {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+    use range::Range;
+    use std::rc::Rc;
+
+    #[test]
+    fn expected_number() {
+        let text = "foo";
+        let chars: Vec<char> = text.chars().collect();
+        let number = Number { property: None };
+        let mut tokenizer = Tokenizer::new();
+        let s = TokenizerState::new();
+        let res = number.parse(&mut tokenizer, &s, &chars, 0);
+        assert_eq!(res, Err((Range::new(0, 0), ParseError::ExpectedNumber)));
+    }
+
+    #[test]
+    fn successful() {
+        let text = "foo 1 1.1 10e1 10.0E1";
+        let chars: Vec<char> = text.chars().collect();
+        let number = Number { property: None };
+        let mut tokenizer = Tokenizer::new();
+        let s = TokenizerState::new();
+        let res = number.parse(&mut tokenizer, &s, &chars[4..], 4);
+        assert_eq!(res, Ok((Range::new(4, 1), s)));
+        let res = number.parse(&mut tokenizer, &s, &chars[6..], 6);
+        assert_eq!(res, Ok((Range::new(6, 3), s)));
+        let res = number.parse(&mut tokenizer, &s, &chars[10..], 10);
+        assert_eq!(res, Ok((Range::new(10, 4), s)));
+
+        let val: Rc<String> = Rc::new("val".into());
+        let number = Number { property: Some(val.clone()) };
+        let res = number.parse(&mut tokenizer, &s, &chars[15..], 15);
+        assert_eq!(res, Ok((Range::new(15, 6), TokenizerState(1))));
+        assert_eq!(tokenizer.tokens.len(), 1);
+        assert_eq!(&tokenizer.tokens[0].0, &MetaData::F64(val.clone(), 10.0e1));
+    }
+}
