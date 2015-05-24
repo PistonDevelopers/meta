cfd42a2881a6a9f1754b9d1ee45e4ef42ee2bba7
Sven Nilsen <bvssvni@gmail.com>
2015-05-24 14:25:21 +0200

    Added parse unit tests for `Select`
    Closes https://github.com/PistonDevelopers/meta/issues/67

--- src/select.rs
+++ src/select.rs
@@ -41,3 +41,51 @@ impl Select {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+    use range::Range;
+    use std::rc::Rc;
+
+    #[test]
+    fn invalid_rule() {
+        let text = "";
+        let chars: Vec<char> = text.chars().collect();
+        let mut tokenizer = Tokenizer::new();
+        let s = TokenizerState::new();
+        let select = Select {
+            args: vec![]
+        };
+        let res = select.parse(&mut tokenizer, &s, &chars, 0);
+        let invalid_rule = match &res {
+            &Err((_, ParseError::InvalidRule(_))) => true,
+            _ => false
+        };
+        assert!(invalid_rule);
+    }
+
+    #[test]
+    fn fail_first() {
+        let text = "2";
+        let chars: Vec<char> = text.chars().collect();
+        let mut tokenizer = Tokenizer::new();
+        let s = TokenizerState::new();
+        let num: Rc<String> = Rc::new("num".into());
+        let select = Select {
+            args: vec![
+                Rule::Text(Text {
+                    allow_empty: true,
+                    property: None
+                }),
+                Rule::Number(Number {
+                    property: Some(num.clone())
+                })
+            ]
+        };
+        let res = select.parse(&mut tokenizer, &s, &chars, 0);
+        assert_eq!(res, Ok((Range::new(0, 1), TokenizerState(1))));
+        assert_eq!(tokenizer.tokens.len(), 1);
+        assert_eq!(&tokenizer.tokens[0].0, &MetaData::F64(num.clone(), 2.0));
+    }
+}
