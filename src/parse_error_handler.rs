6eaab9fff1b517e7264c5230b257ce8eac04fd02
Sven Nilsen <bvssvni@gmail.com>
2015-06-03 15:47:42 +0200

    Include more previous lines when expected token
    Closes https://github.com/PistonDevelopers/meta/issues/142

--- src/parse_error_handler.rs
+++ src/parse_error_handler.rs
@@ -35,12 +35,44 @@ impl<'b> ParseErrorHandler for ParseStdErr<'b> {
     fn error(&mut self, range: Range, error: ParseError) {
         use std::io::{ stderr, Write };
 
+        // Gets the first line of error message.
+        fn first_line(
+            err_handler: &ParseStdErr,
+            range: Range
+        ) -> Option<(usize, Range)> {
+            let mut first_line = None;
+            for (i, &(r, _)) in err_handler.lines.iter().enumerate() {
+                if let Some(intersect) = range.ends_intersect(&r) {
+                    first_line = Some((i, intersect));
+                }
+            }
+            first_line
+        }
+
         let mut stderr = stderr();
         writeln!(&mut stderr, "Error {}", error).unwrap();
+        if let &ParseError::ExpectedToken(_, _) = &error {
+            // Improves the error report when forgetting a token at end of
+            // a line, for example `;` after an expression.
+            if let Some(first_line) = first_line(self, range) {
+                let mut prev_line = 0;
+                for (i, &(_, text)) in
+                    self.lines[..first_line.0].iter().enumerate().rev() {
+                    prev_line = i;
+                    if !text.chars()
+                        .all(|c| { c.is_whitespace() }) { break; }
+                }
+                for (i, &(_, text)) in
+                    self.lines[prev_line .. first_line.0].iter().enumerate() {
+                    writeln!(&mut stderr, "{}: {}",
+                        i + prev_line + 1, text).unwrap();
+                }
+            }
+        }
         for (i, &(r, text)) in self.lines.iter().enumerate() {
             if let Some(intersect) = range.ends_intersect(&r) {
                 writeln!(&mut stderr, "{}: {}", i + 1, text).unwrap();
-                if intersect.offset > r.offset {
+                if intersect.offset >= r.offset {
                     write!(&mut stderr, "{}: ", i + 1).unwrap();
                     let i = intersect.offset - r.offset;
                     for _ in 0 .. i {
