5a155afe9fdf89f940ac43e9044a862b36d87784
Sven Nilsen <bvssvni@gmail.com>
2015-07-19 22:11:28 +0200

    Added `load_syntax_data`
    Closes https://github.com/PistonDevelopers/meta/issues/237

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@
 extern crate read_token;
 extern crate range;
 
-pub use parse_error_handler::{ stderr_unwrap, ParseErrorHandler, ParseStdErr };
+pub use parse_error_handler::{
+    stderr_unwrap,
+    ParseErrorHandler,
+    ParseStdErr
+};
 pub use parse_error::ParseError;
 pub use meta_rules::{ parse, Rule };
 pub use search::Search;
@@ -14,6 +18,8 @@ pub use search::Search;
 pub type DebugId = usize;
 
 use std::rc::Rc;
+use std::fs::File;
+use std::path::Path;
 use range::Range;
 
 pub mod bootstrap;
@@ -54,5 +60,27 @@ pub fn syntax(rules: &str) -> Result<Vec<(Rc<String>, Rule)>, (Range, ParseError
         Err(()) => Err((Range::empty(0), ParseError::Conversion(
             format!("Bootstrapping rules are incorrect"))))
     }
+}
+
+/// Convenience method for loading data.
+/// Panics if there is an error, and writes error message to
+/// standard error output.
+pub fn load_syntax_data<A, B>(
+    syntax_path: A,
+    data_path: B
+) -> Vec<(Range, MetaData)>
+    where A: AsRef<Path>, B: AsRef<Path>
+{
+    use std::io::Read;
+
+    let mut syntax_file = File::open(syntax_path).unwrap();
+    let mut s = String::new();
+    syntax_file.read_to_string(&mut s).unwrap();
+    let rules = stderr_unwrap(&s, syntax(&s));
+
+    let mut data_file = File::open(data_path).unwrap();
+    let mut d = String::new();
+    data_file.read_to_string(&mut d).unwrap();
+    stderr_unwrap(&d, parse(&rules, &d))
 }
 
