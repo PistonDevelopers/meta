530fd632ccf3ba08656b2cfae168fcf4fbdaf0df
Sven Nilsen <bvssvni@gmail.com>
2015-09-17 21:52:35 +0200

    Added more comments

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,13 +3,13 @@ extern crate piston_meta;
 use piston_meta::*;
 
 fn main() {
-    let text = r#"say "Hello world!""#;
-    let rules = r#"1 "rule" ["say" w! t?"foo"]"#;
+    let text = r#"hi James!"#;
+    let rules = r#"
+        1 "say_hi" ["hi" w? {"James""james" "Peter""peter"} "!"]
+        2 "document" @"say_hi"
+    "#;
     // Parse rules with meta language and convert to rules for parsing text.
     let rules = stderr_unwrap(rules, syntax(rules));
     let data = stderr_unwrap(text, parse(&rules, text));
-    assert_eq!(data.len(), 1);
-    if let &MetaData::String(_, ref hello) = &data[0].1 {
-        println!("{}", hello);
-    }
+    json::print(&data);
 }
