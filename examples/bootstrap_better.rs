edb77a5d9c039c2cabca5a06359b109b01ebbde7
Sven Nilsen <bvssvni@gmail.com>
2015-09-17 16:35:25 +0200

    Bootstrap better syntax

--- examples/bootstrap_better.rs
+++ examples/bootstrap_better.rs
@@ -1,5 +1,5 @@
 /*
-Bootstraps meta language to a subset of BNF syntax
+Bootstraps meta language to a better syntax
 */
 
 extern crate piston_meta;
@@ -13,32 +13,32 @@ fn main() {
     // Get the rules built into piston-meta.
     let rules = bootstrap::rules();
 
-    // Load the EBNF syntax.
-    let ebnf_syntax: PathBuf = "assets/ebnf-syntax.txt".into();
-    let mut file_h = File::open(ebnf_syntax).unwrap();
+    // Load the better syntax.
+    let better_syntax: PathBuf = "assets/better-syntax.txt".into();
+    let mut file_h = File::open(better_syntax).unwrap();
     let mut source = String::new();
     file_h.read_to_string(&mut source).unwrap();
     let res = stderr_unwrap(&source, parse(&rules, &source));
     let mut ignored1 = vec![];
-    let ebnf_rules = bootstrap::convert(&res, &mut ignored1).unwrap();
-    println!("ignored EBNF {:?}", ignored1.len());
+    let better_rules = bootstrap::convert(&res, &mut ignored1).unwrap();
+    println!("better-syntax.txtx: ignored {:?}", ignored1.len());
 
-    // Use the EBNF rules to read EBNF self syntax.
-    let ebnf_self_syntax: PathBuf = "assets/ebnf-self-syntax.txt".into();
-    let mut file_h = File::open(ebnf_self_syntax).unwrap();
+    // Use the better rules to read better self syntax.
+    let better_self_syntax: PathBuf = "assets/better-self-syntax.txt".into();
+    let mut file_h = File::open(better_self_syntax).unwrap();
     let mut source = String::new();
     file_h.read_to_string(&mut source).unwrap();
-    let res = stderr_unwrap(&source, parse(&ebnf_rules, &source));
+    let res = stderr_unwrap(&source, parse(&better_rules, &source));
     let mut ignored1 = vec![];
     let rules1 = bootstrap::convert(&res, &mut ignored1).unwrap();
-    println!("ignored1 {:?}", ignored1.len());
+    println!("better-self-syntax.txt: ignored1 {:?}", ignored1.len());
 
-    // Read EBNF self syntax again, using its own rules.
+    // Read better self syntax again, using its own rules.
     let res = stderr_unwrap(&source, parse(&rules1, &source));
     let mut ignored2 = vec![];
     let rules2 = bootstrap::convert(&res, &mut ignored2).unwrap();
-    println!("ignored2 {:?}", ignored2.len());
+    println!("better-self-syntax.txt: ignored2 {:?}", ignored2.len());
     let _ = stderr_unwrap(&source, parse(&rules2, &source));
     assert_eq!(rules1, rules2);
-    println!("Bootstrapping EBNF succeeded!");
+    println!("Bootstrapping better syntax succeeded!");
 }
