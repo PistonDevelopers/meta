4661eb572ef319e6367021cbaa6108a32e2fe7a6
Sven Nilsen <bvssvni@gmail.com>
2017-01-11 14:49:15 +0100

    Added "bench_deep_string"

--- src/lib.rs
+++ src/lib.rs
@@ -161,17 +161,65 @@ mod tests {
             // Parse rules with meta language and convert to rules for parsing text.
             let rules = match syntax_errstr(rules) {
                 Err(err) => {
-                    println!("{}", err);
-                    return;
+                    panic!("{}", err);
                 }
                 Ok(rules) => rules
             };
             let mut data = vec![];
             match parse_errstr(&rules, text, &mut data) {
                 Err(err) => {
-                    println!("{}", err);
-                    return;
+                    panic!("{}", err);
                 }
+                Ok(()) => {}
+            };
+        });
+    }
+
+    #[bench]
+    fn bench_deep_string(b: &mut Bencher) {
+        b.iter(|| {
+            let text = r#"hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+            foo"#;
+            let rules = r#"
+                1 foo = [.." "!:"foo" .w! {"foo" foo}]
+                2 document = foo
+            "#;
+            // Parse rules with meta language and convert to rules for parsing text.
+            let rules = match syntax_errstr(rules) {
+                Err(err) => panic!("{}", err),
+                Ok(rules) => rules
+            };
+            let mut data = vec![];
+            match parse_errstr(&rules, text, &mut data) {
+                Err(err) => panic!("{}", err),
                 Ok(()) => {}
             };
         });
